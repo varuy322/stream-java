3e30cffa2475f641ed0d5b855e60936c0f70e7a7
zhanghan13 <zhanghan13@meituan.com>
2018-04-16 11:22:19 +0800

    ad

--- storm/storm-dynamicJob/src/main/java/com/sdu/stream/storm/schema/RTDJSONConf.java
+++ storm/storm-dynamicJob/src/main/java/com/sdu/stream/storm/schema/RTDJSONConf.java
@@ -2,9 +2,15 @@ package com.sdu.stream.storm.schema;
 
 public class RTDJSONConf implements RTDConf<JSONSchema> {
 
+    private JSONSchema schema;
+
+    public RTDJSONConf(JSONSchema schema) {
+        this.schema = schema;
+    }
+
     @Override
     public JSONSchema dataSchema() {
-        return null;
+        return schema;
     }
 
 }
---
+++ storm/storm-dynamicJob/src/main/java/com/sdu/stream/storm/schema/RTDOperation.java
@@ -0,0 +1,10 @@
+package com.sdu.stream.storm.schema;
+
+/**
+ * @author hanhan.zhang
+ * */
+public interface RTDOperation {
+
+
+
+}
